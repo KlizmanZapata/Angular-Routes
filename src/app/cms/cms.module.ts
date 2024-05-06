e054abb14960bfde9e60269102f3a6daa9b16a34
KlizmanZapata <klizmanzapata117@gmail.com>
2024-05-06 10:54:28 -0500

    Subiendo todo el proyecto finalizado

--- src/app/cms/cms.module.ts
+++ src/app/cms/cms.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CmsRoutingModule } from './cms-routing.module';
-import { TaskComponent } from './pages/task/task.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 import { GridComponent } from './pages/grid/grid.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
 
 @NgModule({
   declarations: [
-    TaskComponent,
+    TasksComponent,
     GridComponent,
     LayoutComponent
   ],
